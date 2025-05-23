1582ceb0e66a2e6d2e0657b80e1bd005407d3885
HyperN <94728590+hyrnxs@users.noreply.github.com>
2025-05-23 17:44:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 # Running update
 sudo apt update && sudo apt upgrade -y
 # Install essential apps
-sudo apt install qemu-system uml-utilities virt-manager git wget libguestfs-tools p7zip-full make tesseract-ocr tesseract-ocr-eng genisoimage vim net-tools screen neofetch -y
+sudo apt install qemu-system uml-utilities git wget libguestfs-tools p7zip-full make tesseract-ocr tesseract-ocr-eng vim net-tools screen neofetch -y
 # Getting virtual machine packages
 sudo wget -O bios.fd "https://archive.org/download/qemu_bios/bios.fd" # Getting Bios
 sudo wget -O loader.qcow2 "https://archive.org/download/qemu_bios/loader.qcow2" # Getting Loader
 sudo wget -O win.iso "https://archive.org/download/win-10-superlite-v.-6/Win10SUPERLITE%20V.6.iso" # Getting Windows ISO
 git clone https://github.com/novnc/noVNC.git noVNC
 qemu-img create -f qcow2 win.img 64G
-# Launching VM
+# Modify & Launching VM
 clear
+echo Modify your VM options, Exit to use default options.
+wait 6
+sudo nano ./vm.sh
+echo Modified.
+wait 6
 echo Launching VM...
+wait 3
 echo "To run the VM again, type "sudo bash vm.sh""
 sudo bash vm.sh
