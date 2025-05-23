d7702e2f2be81fe28b4aa7457cbd9b10a1ef7b1d
HyperN <94728590+hyrnxs@users.noreply.github.com>
2025-05-23 16:58:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ sudo apt install qemu-system uml-utilities virt-manager git wget libguestfs-tool
 # Getting virtual machine packages
 sudo wget -O bios.fd "https://archive.org/download/qemu_bios/bios.fd" # Getting Bios
 sudo wget -O loader.qcow2 "https://archive.org/download/qemu_bios/loader.qcow2" # Getting Loader
-sudo wget -O win.iso "https://archive.org/download/en-us_windows_10_iot_enterprise_ltsc_2021_x64_dvd_257ad90f_202211/en-us_windows_10_iot_enterprise_ltsc_2021_x64_dvd_257ad90f.iso" # Getting Windows ISO
+sudo wget -O win.iso "https://archive.org/download/win-10-superlite-v.-6/Win10SUPERLITE%20V.6.iso" # Getting Windows ISO
 git clone https://github.com/novnc/noVNC.git noVNC
+qemu-img create -f qcow2 win.img 64G
 # Launching VM
 clear
 echo Launching VM...
