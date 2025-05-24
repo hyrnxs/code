45ac9bbb9718fb65108747a3b1e03ffedceb8453
HyperN <94728590+hyrnxs@users.noreply.github.com>
2025-05-24 14:05:12 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -34,7 +34,7 @@ args=(
   -drive id=HDD,if=none,file="./win.img",format=qcow2
   -device ide-hd,bus=sata.4,drive=HDD
   -monitor stdio
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  # -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   -device vmware-svga,id=video0,vgamem_mb=8192,bus=pcie.0,addr=0x10 # Prefer VMWare-SVGA
   # -device qxl-vga,id=video0,vgamem_mb=8192,ram_size=8192,vram_size=8192,bus=pcie.0,addr=0x10 # Prefer QXL-VGA
   # -spice port=5900,addr=127.0.0.1,disable-ticketing=on
