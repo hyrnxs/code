e9c91304bcaf04feeeb7c6bf7ed4f33e53d84fff
HyperN <94728590+hyrnxs@users.noreply.github.com>
2025-05-24 14:04:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,31 @@
 # Running update
 sudo apt update && sudo apt upgrade -y
+
 # Install essential apps
-sudo apt install qemu-system uml-utilities git wget libguestfs-tools p7zip-full make tesseract-ocr tesseract-ocr-eng vim net-tools screen neofetch -y
+sudo apt install qemu-system uml-utilities git wget libguestfs-tools p7zip-full make tesseract-ocr tesseract-ocr-eng vim net-tools screen neofetch -y # Some of them are require
+
 # Getting virtual machine packages
-sudo wget -O bios.fd "https://archive.org/download/qemu_bios/bios.fd" # Getting Bios
+sudo wget -O bios.fd "https://archive.org/download/qemu_bios/bios.fd" # Getting BIOS
 sudo wget -O loader.qcow2 "https://archive.org/download/qemu_bios/loader.qcow2" # Getting Loader
-sudo wget -O win.iso "https://archive.org/download/win-10-superlite-v.-6/Win10SUPERLITE%20V.6.iso" # Getting Windows ISO
+sudo wget -O OVMF_VARS.fd "https://archive.org/download/qemu_bios/OVMF_VARS.fd" # Getting BIOS Vars
+
+# Prompt for ISO link, fuck windows (idk)
+clear
+echo Find a vaild link of OS ISO
+read -p "ISO Link: " ISO_LINK
+sudo wget -O cd.iso "$ISO_LINK" # Getting ISO ############################# done
+
 git clone https://github.com/novnc/noVNC.git noVNC
-qemu-img create -f qcow2 win.img 64G
+qemu-img create -f qcow2 hdd.img 64G # 64 gigabyte? man.
+
 # Modify & Launching VM
 clear
-echo Modify your VM options, Exit to use default options.
-wait 6
+echo "Modify your VM options, Exit to use default options." # modify the cpu and ram are recommended lol
+sleep 6
 sudo nano ./vm.sh
-echo Modified.
-wait 6
-echo Launching VM...
-wait 3
-echo "To run the VM again, type "sudo bash vm.sh""
+echo "Modified."
+sleep 6
+echo "Launching VM..."
+sleep 3
+echo "To run the VM again, type \"sudo bash vm.sh\""
 sudo bash vm.sh
